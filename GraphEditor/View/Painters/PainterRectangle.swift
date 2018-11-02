ba6215a3a2e20be41635e3b90b98bddce483d92e
Dragoslav Ignjatovic <draza021@gmail.com>
2018-11-02 14:57:52 +0100

    completed painter rectangle method with text drawing

--- GraphEditor/View/Painters/PainterRectangle.swift
+++ GraphEditor/View/Painters/PainterRectangle.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class PainterRectangle: SymbolPainter {
     
+    // MARK: - Override method
     override func draw(context: CGContext) {
         context.saveGState()
         context.setFillColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
@@ -26,8 +27,37 @@ class PainterRectangle: SymbolPainter {
         let contextRect = CGRect(x: transformedPosition.x + 2, y: transformedPosition.y + 2, width: transformedSize.width - 4, height: transformedSize.height - 4)
         context.fill(contextRect)
         
-        // draw text here
-        
+        drawCenteredTextWithContext(context, inRectangle: contextRect)
         context.restoreGState()
+    }
+}
+
+// MARK: - Private func
+extension PainterRectangle {
+    private func drawCenteredTextWithContext(_ context: CGContext, inRectangle contextRect: CGRect) {
+        let yOffset = contextRect.origin.y + (contextRect.size.height) / 2.0 - 8
+        let textRect = CGRect(x: contextRect.origin.x, y: yOffset, width: contextRect.size.width, height: contextRect.size.height)
+        
+        guard let transform = ServiceRegistry.sharedInstance.context.transform else { return }
+        let textTransform = CGAffineTransform(a: transform.a, b: transform.b, c: transform.c, d: -transform.d, tx: transform.tx, ty: transform.ty)
+        context.textMatrix = textTransform
+        
+        let text = symbol.text
+        let attributedString = getAttributedString(text)
+        attributedString.draw(in: textRect)
+    }
+    
+    private func getAttributedString(_ text: String) -> NSAttributedString {
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        
+        let attributes = [
+            NSAttributedString.Key.paragraphStyle: paragraphStyle,
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15.0),
+            NSAttributedString.Key.foregroundColor: UIColor.white
+        ]
+        
+        let attributedString = NSAttributedString(string: text, attributes: attributes)
+        return attributedString
     }
 }
